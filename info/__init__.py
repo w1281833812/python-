e8335bbabe29974ea8b30551582ec92b98dad2db
zhangzz <zhangzz@163.com>
2018-08-22 11:23:47 +0800

    集成日志

--- info/__init__.py
+++ info/__init__.py
@@ -1,3 +1,6 @@
+import logging
+from logging.handlers import RotatingFileHandler
+
 from flask import Flask
 from flask_migrate import Migrate
 from flask_session import Session
@@ -9,6 +12,20 @@ from config import config_dict
 # 将数据库操作对象全局化 方便其他文件操作数据库
 db = None  # type: SQLAlchemy
 sr = None  # type: StrictRedis
+
+
+# 配置日志文件(将日志信息写入到文件中)
+def setup_log():
+    # 设置日志的记录等级
+    logging.basicConfig(level=logging.DEBUG)  # 调试debug级
+    # 创建日志记录器，指明日志保存的路径、每个日志文件的最大大小、保存的日志文件个数上限
+    file_log_handler = RotatingFileHandler("logs/log", maxBytes=1024 * 1024 * 100, backupCount=10)
+    # 创建日志记录的格式 日志等级 输入日志信息的文件名 行数 日志信息
+    formatter = logging.Formatter('%(levelname)s %(pathname)s:%(lineno)d %(message)s')
+    # 为刚创建的日志记录器设置日志记录格式
+    file_log_handler.setFormatter(formatter)
+    # 为全局的日志工具对象（flask app使用的）添加日志记录器
+    logging.getLogger().addHandler(file_log_handler)
 
 
 # 定义函数来封装应用的创建   工厂函数
@@ -33,5 +50,8 @@ def create_app(config_type):
     # 3. 注册蓝图
     from info.modules.home import home_blu
     app.register_blueprint(home_blu)
+
+    # 配置日志文件
+    setup_log()
 
     return app
--- info/modules/home/views.py
+++ info/modules/home/views.py
@@ -1,9 +1,15 @@
+from flask import current_app
 from info import sr
 from info.modules.home import home_blu
+import logging  # python内置的日志模块 可以在控制台中显示日志, 也可以将日志写入到文件中
 
 
 # 2.使用蓝图来装饰路由
 @home_blu.route('/')
 def index():
-    sr.set("age", 20)
+    # logging.error("发现了一个错误")  # 显示效果不友好
+    try:
+        1 / 0
+    except BaseException as e:
+        current_app.logger.error("发现了一个错误: %s" % e)  # 使用flask内置的日志表达形式(显示行号)
     return 'index'
