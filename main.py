4535d133b7197d7cc69a0def36bbd91a7f9e35f9
zhangzz <zhangzz@163.com>
2018-08-22 08:58:31 +0800

    redis配置

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from redis import StrictRedis
 
 
 class Config:  # 自定义配置类
     DEBUG = True  # 开启调试模式
     SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/info16"  # mysql连接地址
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # 是否追踪数据库变化
+    REDIS_HOST = "127.0.0.1"  # redis的ip
+    REDIS_PORT = 6379  # redis的端口
 
 
 app = Flask(__name__)
@@ -13,10 +16,13 @@ app = Flask(__name__)
 app.config.from_object(Config)
 # 创建数据库连接对象
 db = SQLAlchemy(app)
+# 创建redis连接对象
+sr = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
 
 
 @app.route('/')
 def index():
+    sr.set("name", "zs")
     return 'index'
 
 
