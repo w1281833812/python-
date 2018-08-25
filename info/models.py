90c8006996de7b4958ab106df390c4bf6880f386
zhangzz <zhangzz@163.com>
2018-08-25 09:56:38 +0800

    登录信息显示

--- info/models.py
+++ info/models.py
@@ -58,7 +58,7 @@ class User(BaseModel, db.Model):
     news_list = db.relationship('News', backref='user', lazy='dynamic')
 
 
-    def to_dict(self):
+    def to_dict(self):  # 对一些模型数据进行格式化处理(封装展示效果)
         resp_dict = {
             "id": self.id,
             "nick_name": self.nick_name,
--- info/modules/home/views.py
+++ info/modules/home/views.py
@@ -1,12 +1,24 @@
+from info.models import User
 from info.modules.home import home_blu
-from flask import render_template, current_app
+from flask import render_template, current_app, session
 
 
 # 2.使用蓝图来装饰路由
 @home_blu.route('/')
 def index():
+    # 判断用户是否登录
+    user_id = session.get("user_id")
+    user = None
+    if user_id:
+        # 根据user_id查询用户模型
+        try:
+            user = User.query.get(user_id)
+        except BaseException as e:
+            current_app.logger.error(e)
 
-    return render_template("index.html")
+    user = user.to_dict() if user else None
+    # 将用户登录信息传到模板中
+    return render_template("index.html", user=user)
 
 
 # 设置图标
