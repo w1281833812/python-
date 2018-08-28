b4fca93ba5a19fb99124ae159229aefe0f7a1b68
zhangzz <zhangzz@163.com>
2018-08-28 12:33:46 +0800

    显示基本信息

--- info/modules/user/views.py
+++ info/modules/user/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, g, redirect
+from flask import render_template, g, redirect, abort
 
 from info.common import user_login_data
 from info.modules.user import user_blu
@@ -14,3 +14,13 @@ def user_info():
 
     user = user.to_dict() if user else None
     return render_template("news/user.html", user=user)
+
+
+@user_blu.route('/base_info')
+@user_login_data
+def base_info():
+    user = g.user
+    if not user:
+        return abort(404)
+
+    return render_template("news/user_base_info.html")
