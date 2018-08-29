150ff3352c3dcf38aaf19cdb908b0a5a84da1d9f
zhangzz <zhangzz@163.com>
2018-08-29 12:35:41 +0800

    生成超级管理员命令

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from flask import session
+from flask import current_app
 from flask_script import Manager
 from flask_migrate import MigrateCommand
 from info import create_app
-
 
 # 创建应用
 app = create_app("dev")
@@ -12,5 +11,34 @@ mgr = Manager(app)
 mgr.add_command("mc", MigrateCommand)
 
 
+# 生成超级管理员
+@mgr.option("-u", dest="username")
+@mgr.option("-p", dest="password")
+def create_superuser(username, password):
+    if not all([username, password]):
+        print("账号/密码不完整")
+        return
+
+    from info.models import User
+    from info import db
+    # 创建用户模型
+    user = User()
+    user.mobile = username
+    user.password = password
+    user.nick_name = username
+    user.is_admin = True
+
+    try:
+        db.session.add(user)
+        db.session.commit()
+    except BaseException as e:
+        current_app.logger.error(e)
+        db.session.rollback()
+        print("生成失败")
+        return
+
+    print("生成管理员成功")
+
+
 if __name__ == '__main__':
-   mgr.run()
+    mgr.run()
