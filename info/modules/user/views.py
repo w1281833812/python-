2511fe5f0b1961aa7492d92c9c3267ed6bcf3f25
zhangzz <zhangzz@163.com>
2018-08-29 10:50:56 +0800

    显示/修改密码

--- info/modules/user/views.py
+++ info/modules/user/views.py
@@ -77,3 +77,29 @@ def pic_info():
     user.avatar_url = file_name
 
     return jsonify(errno=RET.OK, errmsg=error_map[RET.OK], data=user.to_dict())
+
+
+# 显示/修改密码
+@user_blu.route('/pass_info', methods=['GET', 'POST'])
+@user_login_data
+def pass_info():
+    user = g.user
+    if not user:
+        return abort(404)
+
+    if request.method == "GET":
+        return render_template("news/user_pass_info.html")
+    # POST处理
+    old_password = request.json.get("old_password")
+    new_password = request.json.get("new_password")
+    if not all([old_password, new_password]):
+        return jsonify(errno=RET.PARAMERR, errmsg=error_map[RET.PARAMERR])
+
+    # 校验旧密码是否正确
+    if not user.check_password(old_password):
+        return jsonify(errno=RET.PWDERR, errmsg=error_map[RET.PWDERR])
+
+    # 修改密码
+    user.password = new_password
+
+    return jsonify(errno=RET.OK, errmsg=error_map[RET.OK])
